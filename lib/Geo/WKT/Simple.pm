17188723a5ed494dd7a0953123f9b57c44bee0da
Yuto Kawamura <kawamuray@nhn.com>
2012-10-03 12:23:14 +0900

    initial commit

---
+++ lib/Geo/WKT/Simple.pm
@@ -0,0 +1,32 @@
+package Geo::WKT::Simple;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Geo::WKT::Simple -
+
+=head1 SYNOPSIS
+
+  use Geo::WKT::Simple;
+
+=head1 DESCRIPTION
+
+Geo::WKT::Simple is
+
+=head1 AUTHOR
+
+Yuto KAWAMURA(kawamuray) E<lt>kawamuray {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
